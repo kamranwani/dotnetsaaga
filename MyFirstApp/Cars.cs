1bcb63921539cc4020105569828eb76487aea145
kamranwani <kamraanwani@gmail.com>
2025-08-04 15:23:15 +0530

    fix

---
+++ MyFirstApp/Cars.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    internal class Cars
+    {
+        private string _BrandName;
+        private string _CarModel;
+        private string _CarName;
+        private int _Year;
+        private decimal _Price;
+
+        public static int _NumberOfUnits;
+
+
+        public Cars() {
+            _BrandName = "unknown";
+            _CarModel="unknown"; ;
+            _CarName = "unknown";
+            _Year = 0;
+            _Price = 0;
+            _NumberOfUnits++;
+
+        }    
+       public Cars(string brandName, string carModel, string carName, int year, decimal price)
+        {
+            _BrandName = brandName;
+            _CarModel = carModel;
+            _CarName = carName;
+            _Year = year;
+            _Price = price;
+            _NumberOfUnits++;
+        }
+
+       static Cars()
+        {
+            _NumberOfUnits = 0;
+        }
+
+        public Cars(Cars carcopy)
+        {
+            _BrandName = carcopy._BrandName;
+            _CarModel = carcopy._CarModel;
+            _CarName = carcopy._CarName;
+            _Year = carcopy._Year;
+            _Price = carcopy._Price;
+            _NumberOfUnits++;
+        }
+
+        public void SetBrandName(string brandname)
+        {
+            if (!string.IsNullOrWhiteSpace(brandname))
+                _BrandName = brandname;
+            else
+                Console.WriteLine("Invalid brand name");
+        }
+
+        public string GetBrandName()
+        {
+            return _BrandName;
+        }
+        public void SetCarModel(string carmodel)
+        {
+            if (!string.IsNullOrWhiteSpace(carmodel))
+                _CarModel = carmodel;
+            else
+                Console.WriteLine("Invalid car model");
+        }
+
+        public string GetCarModel()
+        {
+            return _CarModel;
+        }
+        public void SetCarName(string carName)
+        {
+            if (!string.IsNullOrWhiteSpace(carName))
+                _CarName = carName;
+            else
+                Console.WriteLine("Invalid car name");
+        }
+
+        public string GetCarName()
+        {
+            return _CarName;
+        }
+
+        public void SetYear(int year)
+        {
+            if (year > 1885 && year <= DateTime.Now.Year) 
+                _Year = year;
+            else
+                Console.WriteLine("Invalid manufacturing year");
+        }
+
+        public int GetYear()
+        {
+            return _Year;
+        }
+
+        public void SetPrice(decimal price)
+        {
+            if (price >= 0)
+                _Price = price;
+            else
+                Console.WriteLine("Invalid price");
+        }
+
+        public decimal GetPrice()
+        {
+            return _Price;
+        }
+
+        public  void ShowCarData()
+        {
+            Console.WriteLine($@"Car Details are:
+Car Brand: {_BrandName}
+Car Model: {_CarModel}
+Car Name: {_CarName}
+Manufacturing Year: {_Year}
+Car Price: {_Price}
+NumberOfCarUnits:{_NumberOfUnits}");
+        }
+    }
+}
--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -6,257 +6,27 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        //program1 here 
-        //Console.WriteLine("Enter your name");
-        //string name = Console.ReadLine();
-        //Console.WriteLine("Enter your age!");
-        //string tempAge= Console.ReadLine();
+       Cars car=new Cars();
+        car.SetBrandName("Hyundai");
+        car.SetCarName("i20 sports");
+        car.SetPrice(104000.11m);
+        car.SetCarModel("i2001ddwKml");
+        car.SetYear(2025);
 
-        //bool isAge = int.TryParse(tempAge, out int age);
-        //if (isAge && age >= 18) {
-        //    Console.WriteLine($@"hello! {name} 
-        //    you're {age} years old and eligible ");
-        //    return;
-        //} else {
-        //    Console.WriteLine($@"{isAge} ,{age} is below 18 so not eligible");
-        //}   
+        Console.WriteLine(car.GetBrandName()); ;
+        
+        //car.ShowCarData();
 
-        //Program 2
-        // Console.WriteLine("Enter length");
-        // string tempLength = Console.ReadLine();
-        // Console.WriteLine("Enter breadth");
-        // string tempWidth = Console.ReadLine();
-        // bool isLength = float.TryParse(tempLength, out float length);
-        // bool isWidth = float.TryParse(tempWidth, out float width);
-        // if (isLength&&isWidth) { 
-        //float area= AreaAndPerimeter.CalArea(length, width);
-        // Console.WriteLine($"Area is {area}");
-        //float perimeter = AreaAndPerimeter.CalPerimeter(length, width);
-        // Console.WriteLine($"Perimeter is {perimeter}");
-        //DisplayingMessage.DisplayMsg(); error internal method
+        Cars bmw=new Cars("bmw","BKLPAJDHAAN","M30D",2024,4521000.22m);
+        //bmw.ShowCarData();
 
-        //Program 3
-        // Calculations calc = new Calculations();
-        //calc.Calculation(200, 300, 400);
+        Cars bmwCopy = bmw;
+        bmwCopy.ShowCarData();
 
-        //program4
-
-        //Calculations cal =new Calculations();
-        //int sum1=cal.Sum(5, 10);
-        //Console.WriteLine(sum1);
-        //float sum2 = cal.Sum(3.5f, 4.5f);
-        //Console.WriteLine(sum2);
-
-        //Program 5
-        //Student[] stud = new Student[3];
-        //for (int i = 0; i < 3; i++)
-        //{
-        //    stud[i] = new Student();
-        //    Console.WriteLine("enter student name");
-        //    stud[i].name = Console.ReadLine();
-        //    stud[i].id = i;
-        //    Student.count++;
-
-        //}
-        //Console.WriteLine($"Total Students are {Student.count}");
-
-        //Program6
-        //Employee emp1 = new Employee();
-        //Employee emp2 = new Employee();
-
-        //emp2.age = 30;
-
-
-        //Console.WriteLine($"emp1 age is {emp1.age} and emp2 age is {emp2.age}");
-
-        //Program 7
-        //Console.WriteLine($"Enter Number for factorial");
-        //string TempFactorialNumberInput = Console.ReadLine();
-        //bool isValid = int.TryParse(TempFactorialNumberInput, out int FactNumber);
-        //if (isValid && FactNumber >= 0) {
-        //    int result = Factorial(FactNumber);
-        //    Console.WriteLine($"Factorial of {FactNumber} is: {result}");
-        //}
-        //else
-        //{ Console.WriteLine("Please enter valid input to generate factorial");
-        //}
-
-        //Program 8 
-        //Area CalculateArea = new Area();
-        //float squareArea = CalculateArea.CalArea(3);
-        //Console.WriteLine(squareArea);
-
-        //float rectangleArea = CalculateArea.CalArea(3,5);
-        //Console.WriteLine(rectangleArea);
-
-        //float circleArea = CalculateArea.CalArea(3, true);
-        //Console.WriteLine(circleArea);
-
-        //Program 9 
-
-        //Student kamran = new Student();
-        //Student chatgpt = new Student("chatgpt", 29);
-
-        //Student third = new Student(chatgpt);
-
-        //chatgpt.DisplayData();
-        //third.DisplayData();
-
-
-        //third.name = "kamran";
-        //third.DisplayData();
-
-
-        //chatgpt.name = "GPT";
-        //chatgpt.DisplayData();
-
-        //Program 10 
-
-        //Book book1 = new Book();
-        //book1.DisplayBook();
-        //Book book2 = new Book("Atomic Habits", "James Clear", 2018);
-        //book2.DisplayBook();
-        //Book book3 = new Book(book2);
-        //book3.DisplayBook();
-
-        //book3.updateField("title", "Deep Work");
-        //book3.DisplayBook();
-
-        //book3.updateField("year", "2020");
-        //book3.DisplayBook();
-
-        //book2.updateField("author", "GPT");
-        //book2.DisplayBook();
-
-        //bool result = book2.IsSameBook(book3);
-        //Console.WriteLine($"Book2 and Book3 are {(result ? "same" : "different")}");
-
-        //Book.ShowTotalbooks();
-
-        //Book[] library = new Book[3];
-        //for (int i = 0; i < library.Length; i++) {
-        //    library[i] = new Book();
-        //    Console.WriteLine("enter title name");
-        //    library[i].title = Console.ReadLine();
-        //    Console.WriteLine("enter author name");
-        //    library[i].author = Console.ReadLine();
-        //    Console.WriteLine("enter year");
-        //    string tempYear = Console.ReadLine();
-        //    bool isYear = int.TryParse(tempYear, out int year);
-        //    if (isYear)
-        //    {
-        //        library[i].year = year;
-        //    }
-        //}
-        //int prevsBookyear = 0;
-
-        //foreach (Book book in library)
-        //{
-
-        //    if (prevsBookyear < book.year)
-        //    {
-        //        prevsBookyear = book.year;
-        //    }
-
-
-
-        //}
-
-        //Book.ShowLatestBook(prevsBookyear, library);
-
-        //Program 11 
-
-        //int smallNum = 100;
-        //long bigNum;
-        //double preciseValue = 1234.56;
-        //int approxValue;
-
-        //bigNum = smallNum;
-        //approxValue = (int)preciseValue;
-        //{
-        //    Console.WriteLine($"{smallNum},{bigNum},{preciseValue},{approxValue}");
-
-
-
-
-
-        //}
-
-        //Program 12 
-
-        //Snake snake = new Snake("cobra", "hiss", null, "reptile");
-        //Console.WriteLine("before shallow copy");
-        //Console.WriteLine("----------");
-        //snake.ShowSnakeData();
-        //Console.WriteLine("----------");
-
-        //Snake snake2 = snake;
-        //snake2.name = "cobra updated";
-        //Snake snake3 = new Snake(snake);
-
-        //snake3.name = "python";
-
-        //Console.WriteLine("after shallow copy");
-
-        //snake.ShowSnakeData();
-        //Console.WriteLine("----------");
-
-        //snake2.ShowSnakeData();
-        //Console.WriteLine("----------");
-        //Console.WriteLine("after deep copy");
-        //Console.WriteLine("----------");
-        //snake3.ShowSnakeData();
-        //Console.WriteLine(Snake.snakeType);
-
-        //Sheep sheep = new Sheep();
-        //Cat cat = new Cat();
-        //Dog dog = new Dog();
-
-        //object []animalsArr = new object[5];
-
-
-        //animalsArr[0] = cat;
-        //animalsArr[1] = dog;
-        //animalsArr[2] = sheep;
-        //animalsArr[3] = snake;
-        //animalsArr[4] = snake3;
-
-        //foreach (object animal in animalsArr)
-        //{
-        //    if (animal is Cat)
-        //    {
-        //        Cat myCat = animal as Cat;
-        //        Console.WriteLine(myCat != null ? "is a cat" : "got null instead of cat");
-        //    }
-        //    else if (animal is Dog)
-        //    {
-        //        Dog myDog = animal as Dog;
-        //        Console.WriteLine(myDog != null ? "is a dog" : "got null instead of dog");
-        //    }
-        //    else if (animal is Sheep)
-        //    {
-        //        Sheep mySheep = animal as Sheep;
-        //        Console.WriteLine(mySheep != null ? "is a sheep" : "got null instead of sheep");
-        //    }
-        //    else if (animal is Snake) {
-        //        Snake tempSnake = animal as Snake;
-        //        Console.WriteLine(tempSnake != null ? "is a snake" : "got null instead of snake");
-        //    }
-
-        //Program 13 
-        string str1 = "abc";
-        string str2 = null;
-
-        Console.WriteLine(str2);
-
-
-            //part of program 7
-            //    public static int Factorial(int num) {
-            //    if (num == 0 || num == 1)
-            //        return 1;
-            //    return num * Factorial(num - 1);
-            //}
-        }
+        Cars mercedes = new Cars(bmw);
+        mercedes.SetBrandName("mercedes");
+        mercedes.ShowCarData();
+    }
     }
 
 
