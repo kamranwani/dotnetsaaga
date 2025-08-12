862f7800590fe59458b9e518373538c7bdad6aba
kamranwani <kamraanwani@gmail.com>
2025-08-12 16:14:53 +0530

    fix

---
+++ MyFirstSolution.Domains/GamingLaptop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSolution.Domains;
+
+public class GamingLaptop : Laptop
+{
+
+    public GamingLaptop() {
+        _brand = "Asus Rog";
+        Price = 112000.00m;
+    }
+
+    public void PrintX()
+    {
+        Console.WriteLine(x);
+    }
+}
---
+++ MyFirstSolution.Domains/Laptop.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstSolution.Domains
+{
+    public class Laptop
+    {
+        protected string _brand;
+        private decimal _price;
+        protected int x = 10;
+
+        public decimal Price
+        {
+            get { return _price; }
+            set { 
+                if (value > 100m)
+                    _price = value;
+                else
+                    Console.WriteLine("Enter a valid price above RS 100");
+            }
+        }
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine($"Laptop Details are Brand Name:{_brand } Price:{Price}");
+        }
+    } 
+}
